3ffe7024c806ca5f28441646d6a2b1ff60f210cc
lauvsong <hyehye9355@naver.com>
2022-12-19 15:09:46 +0900

    :sparkles: 조건문 테스트

---
+++ src/main/kotlin/statementtest/StatementTest.kt
@@ -0,0 +1,67 @@
+package statementtest
+
+import com.sun.org.apache.xpath.internal.operations.Bool
+
+fun main() {
+
+}
+
+fun validateScoreIsNotNegative(score: Int) {
+    if (score !in 0..100) {
+        throw IllegalArgumentException("${score}의 범위는 0부터 100입니다.")
+    }
+}
+
+fun getPassOrFail(score: Int): String {
+    return if (score >= 50) {
+        "P"
+    } else {
+        "F"
+    }
+}
+
+fun getGrade(score: Int): String {
+    return if (score >= 90) {
+        return "A"
+    } else if (score >= 80) {
+        return "B"
+    } else if (score >= 70) {
+        return "C"
+    } else if (score >= 60) {
+        return "D"
+    } else {
+        return "F"
+    }
+}
+
+fun getGradeWithSwitch(score: Int): String {
+    return when (score) {
+        in 90..99 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        in 60..69 -> "D"
+        else -> "F"
+    }
+}
+
+fun startsWithA(obj: Any): Boolean {
+    return when (obj) {
+        is String -> obj.startsWith("A")
+        else -> false
+    }
+}
+
+fun judgeNumber(number: Int) {
+    when (number) {
+        1,0,-1 -> println("어디서 많이 본 숫자입니다.")
+        else -> println("-1, 0, 1이 아닙니다.")
+    }
+}
+
+fun judgeNumber2(number: Int) {
+    when {
+        number == 0 -> println("주어진 숫자는 0입니다")
+        number % 2 == 0 -> println("주어진 숫자는 짝수입니다.")
+        else -> println("주어진 숫자는 홀수입니다.")
+    }
+}
